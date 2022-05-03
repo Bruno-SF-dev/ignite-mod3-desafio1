87a05465ef05cd19582991b0a7ca241f26e82741
Bruno Santos <brunosantosfarias2@gmail.com>
2022-05-02 23:16:44 -0300

    renderizar posts

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,15 @@
 import { GetStaticProps } from 'next';
+import Link from 'next/link';
+import Head from 'next/head';
+import { useState } from 'react';
 
 import { getPrismicClient } from '../services/prismic';
+import { format } from 'date-fns';
+import { FiCalendar, FiUser } from 'react-icons/fi';
 
 import commonStyles from '../styles/common.module.scss';
 import styles from './home.module.scss';
+import { ptBR } from 'date-fns/locale';
 
 interface Post {
   uid?: string;
@@ -24,13 +30,70 @@ interface HomeProps {
   postsPagination: PostPagination;
 }
 
-// export default function Home() {
-//   // TODO
-// }
+export default function Home({ postsPagination }: HomeProps) {
+  console.log(postsPagination);
 
-// export const getStaticProps = async () => {
-//   // const prismic = getPrismicClient({});
-//   // const postsResponse = await prismic.getByType(TODO);
+  const [posts, setPosts] = useState<Post[]>(postsPagination.results);
 
-//   // TODO
-// };
+  return (
+    <>
+      <Head>
+        <title>Início</title>
+      </Head>
+      <main className={`${styles.container} ${commonStyles.maxContainer}`}>
+        <div className={styles.posts}>
+          {posts.map(post => (
+            <Link href="/" key={post.uid}>
+              <a>
+                <strong>{post.data.title}</strong>
+                <p>{post.data.subtitle}</p>
+                <div className={styles.info}>
+                  <span>
+                    <FiCalendar color="#BBBBBB" size={20} />{' '}
+                    {post.first_publication_date}
+                  </span>
+                  <span>
+                    <FiUser color="#BBBBBB" size={20} /> {post.data.author}
+                  </span>
+                </div>
+              </a>
+            </Link>
+          ))}
+        </div>
+        <button className={styles.loadMoreButton}>Carregar mais posts</button>
+      </main>
+    </>
+  );
+}
+
+export const getStaticProps: GetStaticProps = async ({ previewData }) => {
+  const prismic = getPrismicClient({});
+
+  const postsResponse = await prismic.getByType('desafio-posts', {
+    pageSize: 2,
+  });
+
+  const results = postsResponse.results.map(result => ({
+    first_publication_date: format(
+      new Date(result.first_publication_date),
+      'dd MMM yyyy',
+      {
+        locale: ptBR,
+      }
+    ),
+    data: {
+      title: result.data.title[0].text,
+      subtitle: result.data.subtitle[0].text,
+      author: result.data.author[0].text,
+    },
+  }));
+
+  return {
+    props: {
+      postsPagination: {
+        results,
+        next_page: postsResponse.next_page,
+      },
+    },
+  };
+};
